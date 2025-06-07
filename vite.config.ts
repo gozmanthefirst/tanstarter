cd25c8434a1c521391bd7b03f420a52bb8567714
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-06-07 02:40:07 +0100

    change build target

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import tailwindcss from '@tailwindcss/vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
+
   plugins: [
-    tailwindcss(), tsConfigPaths(), tanstackStart()],
-})
+    tailwindcss(),
+    tsConfigPaths(),
+    tanstackStart({
+      target: "vercel",
+    }),
+  ],
+});
